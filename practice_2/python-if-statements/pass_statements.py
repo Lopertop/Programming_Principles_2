560ed09ee8c5ebb13060619a6af618b67d4a1ef8
Eldar <iltem2007@gmail.com>
2026-01-31 15:44:44 +0500

    Added Conditions topic

---
+++ practice_2/python-if-statements/pass_statements.py
@@ -0,0 +1,30 @@
+a = 33
+b = 200
+
+if b > a:
+    pass
+
+age = 16
+
+if age < 18:
+    pass
+else:
+    print("Access granted")
+
+score = 85
+
+if score > 90:
+    pass
+print("Score processed")
+
+value = 50 
+
+if value < 0:
+    print("Negative value")
+elif value == 0:
+    pass
+else:
+    print("Positive value")
+
+def calcukate_discount(price):
+    pass
