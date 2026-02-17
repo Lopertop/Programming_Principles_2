2dd4c9b32294a0a404938778167e0b495b215729
Eldar <iltem2007@gmail.com>
2026-02-17 11:15:33 +0500

    Added iterators topic

---
+++ practice_4/python-generators/iterators.py
@@ -0,0 +1,18 @@
+my_tuple = ("apple", "banana", "cherry")
+myit = iter(my_tuple)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
+print("--------------------")
+
+mystr = "banana"
+myit1 = iter(mystr)
+
+print(next(myit1))
+print(next(myit1))
+print(next(myit1))
+print(next(myit1))
+print(next(myit1))
+print(next(myit1))
---
+++ practice_4/python-generators/looping_through_iterator.py
@@ -0,0 +1,11 @@
+my_tuple = ("apple", "banana", "cherry")
+
+for x in my_tuple:
+    print(x)
+
+print("--------------")
+
+mystr = "banana"
+
+for x in mystr:
+    print(x)
